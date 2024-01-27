f50b1b8a48393620b64ac8430d1775f355e459a1
anonymous-sys19 <estudiosrg5@gmail.com>
2024-01-27 00:03:31 -0600

    new for inplement user and register g f

--- src/routes/ImageUploader.jsx
+++ src/routes/ImageUploader.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-unused-vars */
 // src/components/ImageUploader.jsx
 import { useEffect, useState } from 'react';
 import { supabase } from './Auth/supabaseClient';
 import { UserAuth } from './Auth/AuthContext';
 import { FcUpload } from "react-icons/fc";
+import { FaImages } from "react-icons/fa";
 
+// eslint-disable-next-line no-unused-vars
 import { SuccessNotification, ErrorNotification, WarningNotification } from '../hooks/HooksAlerts';
 
 const Upload = () => {
@@ -40,15 +43,14 @@ const Upload = () => {
 
   const [image, setImage] = useState(null);
   const [description, setDescription] = useState('');
+  // eslint-disable-next-line no-unused-vars
   const [nombreArchivo, setNombreArchivo] = useState('');
 
   const handleImageChange = (e) => {
     const nombreArchivo = e.target.files[0];
     // Limpiar el nombre del archivo: reemplazar espacios con guiones bajos
 
-
-
-    setImage(nombreArchivo);
+    setImage(URL.createObjectURL(nombreArchivo));
     setNombreArchivo(nombreArchivo);
 
 
@@ -132,18 +134,12 @@ const Upload = () => {
 
 
   };
-  // if (isAuthenticated) {
-  //   handleUpload()
-  // }
-  // else {
-  //   <WarningNotification message={"No has iniciado session😑"} />
-  // }
 
 
   return (
     <article className="ArticleUpload" id='ArticleUpload'>
-      <div className="container text-center">
-        <div className="title py-5">
+      <div className="text-center">
+        {/* <div className="title py-5">
           <div className='ContainerUpload'>
             <div>
               <img src="/images/logo-idec.png" style={{
@@ -155,63 +151,65 @@ const Upload = () => {
               <h1>IDEC UPLOAD</h1>
             </div>
           </div>
-        </div>
+        </div> */}
 
         <div className="">
           <div className="cont-Form">
             <form
-              // action="/Upload"
               encType="multipart/form-data"
-              // method="post"
               onSubmit={handleUpload}>
 
-              <div className="">
-                <div className="" style={{
-
-                }}>
-                  <div className="container-input" style={{
-
-                  }}>
-                    <input onChange={handleImageChange} accept='image/*' type="file" name="file-5" id="file-5" className="inputfile inputfile-5" required />
-                    <label htmlFor="file-5">
-                      <figure>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="iborrainputfile" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg>
-                      </figure>
-                      <span className="iborrainputfile">{nombreArchivo.name || 'Seleccionar archivo'}</span>
-                    </label>
-
+              <div className="container-items" >
+                <div className='container-Upload'>
+                  {isAuthenticated ? (
                     <div>
-                      <input type="text" placeholder="Desccription" value={description} onChange={handleDescriptionChange} style={{
-                        margin: '2rem auto',
-                        width: '80%',
-
-                      }} />
+                      {user?.picture && <img className='imgNav' src={user?.picture} alt={user} />}
                     </div>
-                    <div className="colB">
-                      <li style={{ listStyle: 'none' }}>
-
-                        {isAuthenticated ? (
-                          <button
-                            className="btn btn-warning"
-                            type='submit'
-                          >
-                            <FcUpload />
-                            Compartir</button>
-                        ) : (
-                          <a
-                            className="btn btn-warning"
-                            href='/login'
-                          >
-                            <FcUpload />
-                            Inicia session</a>
-                        )}
-
-                      </li>
+                  ) : (
+                    <div style={{
+                      margin: '0 0 0 10px',
+                      bottom: '10px',
+                    }}>
+                      {<img className='imgNav' src="/" alt='' />}
                     </div>
+                  )}
 
-                  </div>
+                </div>
+                <div className='input-Description'>
+                  <input type="text" placeholder={"¿Que estás pensando, " + user.name + "?"} value={description} onChange={handleDescriptionChange} />
+
+                </div>
+                <div className='changeImage'>
+                  <input onChange={handleImageChange} accept='image/*' type="file" name="file-5" id="file-5" className="inputfile inputfile-5" required multiple />
+                  <label htmlFor="file-5">
+                    <figure>
+                      <FaImages className='iborrainputfile' />
+
+                    </figure>
+                    {/* <span className="iborrainputfile">{nombreArchivo.name || ''}</span> */}
+                  </label>
+
+                </div>
+                <div className="btn-share">
+                  <li style={{ listStyle: 'none' }}>
+
+                    {nombreArchivo ? (
+                      isAuthenticated ? (
+                        <button className="btn btn-success" type='submit'>
+                          <FcUpload />  Share
+                        </button>
+                      ) : (
+                        <a className="btn" style={{ color: 'blue', }} href='/register'>
+                          Login
+                        </a>
+                      )
+                    ) : (
+                      ""
+                    )}
+                  </li>
                 </div>
               </div>
+
 
               {/* Condición para mostrar notificaciones */}
               {isSuccess && (
@@ -222,6 +220,21 @@ const Upload = () => {
               )}
 
             </form>
+          </div>
+        </div>
+        <div className='"container text-center'>
+
+        </div>
+        <div className='row'>
+          <div className="col">
+            <div>
+              {nombreArchivo ? (
+                <img className='iborrainputfile' src={image} style={{
+                  widows: 'auto',
+                  height: '-webkit-fit-content'
+                }} />) : ""}
+
+            </div>
           </div>
         </div>
 
