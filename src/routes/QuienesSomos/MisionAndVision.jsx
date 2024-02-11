425d3329877f21759a416a1c8591ec10b348aa6c
anonymous-sys19 <estudiosrg5@gmail.com>
2024-02-11 01:50:31 -0600

    of Css

--- src/routes/QuienesSomos/MisionAndVision.jsx
+++ src/routes/QuienesSomos/MisionAndVision.jsx
@@ -1,45 +1,49 @@
-
-const MisionAndVision = () => {
+import '/public/css/historia.css'
+const MisionAndVision = () => {88
   return (
-    <div>
-      <section>
-        <h2>Misión</h2>
-        <p>
-          La Iglesia de Dios E.C. de Guatemala existe para perpetuar el evangelio completo de Jesucristo
-          (Mateo 28:19-20) en el Espíritu y poder Pentecostés (Hechos 2:1-4,6,13-18), a través de: la comunión,
-          la adoración, el testimonio, la proclamación, la enseñanza y el servicio.
-        </p>
-      </section>
+    <article>
+      <div className="container cont1">
+        <div className="container">
+          <div className="container">
+            <h2>Misión</h2>
+            <p className="">
+              La Iglesia de Dios E.C. de Guatemala existe para perpetuar el evangelio completo de Jesucristo
+              (Mateo 28:19-20) en el Espíritu y poder Pentecostés (Hechos 2:1-4,6,13-18), a través de: la comunión,
+              la adoración, el testimonio, la proclamación, la enseñanza y el servicio.
+            </p>
+          </div>
 
-      <section>
-        <h2>Visión</h2>
-        <p>
-          Ser una Iglesia fundamentada en las Sagradas Escrituras, llena del Espíritu Santo, en constante crecimiento;
-          que adora a Dios en espíritu y verdad; que tiene una profunda pasión por los no alcanzados, un compromiso con
-          la evangelización, el discipulado, la plantación de iglesias y la Misión Global; que desarrolla integralmente a
-          sus ministros y congregaciones; que sirve a los que sufren y produce un impacto transformador en la sociedad.
-        </p>
-        <hr />
-      </section>
-      <section>
-        
-        <h2>Plan Estratégico - Conexión 20/30</h2>
-        <h3>Misión</h3>
-        <p>
-          La Iglesia de Dios E.C. de Guatemala existe para perpetuar el evangelio completo de Jesucristo
-          (Mateo 28:19-20) en el Espíritu y poder Pentecostés (Hechos 2:1-4,6,13-18), a través de: la comunión,
-          la adoración, el testimonio, la proclamación, la enseñanza y el servicio.
-        </p>
+          <div>
+            <h2>Visión</h2>
+            <p>
+              Ser una Iglesia fundamentada en las Sagradas Escrituras, llena del Espíritu Santo, en constante crecimiento;
+              que adora a Dios en espíritu y verdad; que tiene una profunda pasión por los no alcanzados, un compromiso con
+              la evangelización, el discipulado, la plantación de iglesias y la Misión Global; que desarrolla integralmente a
+              sus ministros y congregaciones; que sirve a los que sufren y produce un impacto transformador en la sociedad.
+            </p>
+            <hr />
+          </div>
+          <div>
 
-        <h3>Visión</h3>
-        <p>
-          Ser una Iglesia fundamentada en las Sagradas Escrituras, llena del Espíritu Santo, en constante crecimiento;
-          que adora a Dios en espíritu y verdad; que tiene una profunda pasión por los no alcanzados, un compromiso con
-          la evangelización, el discipulado, la plantación de iglesias y la Misión Global; que desarrolla integralmente a
-          sus ministros y congregaciones; que sirve a los que sufren y produce un impacto transformador en la sociedad.
-        </p>
-      </section>
-    </div>
+            <h2>Plan Estratégico - Conexión 20/30</h2>
+            <h3>Misión</h3>
+            <p>
+              La Iglesia de Dios E.C. de Guatemala existe para perpetuar el evangelio completo de Jesucristo
+              (Mateo 28:19-20) en el Espíritu y poder Pentecostés (Hechos 2:1-4,6,13-18), a través de: la comunión,
+              la adoración, el testimonio, la proclamación, la enseñanza y el servicio.
+            </p>
+
+            <h3>Visión</h3>
+            <p>
+              Ser una Iglesia fundamentada en las Sagradas Escrituras, llena del Espíritu Santo, en constante crecimiento;
+              que adora a Dios en espíritu y verdad; que tiene una profunda pasión por los no alcanzados, un compromiso con
+              la evangelización, el discipulado, la plantación de iglesias y la Misión Global; que desarrolla integralmente a
+              sus ministros y congregaciones; que sirve a los que sufren y produce un impacto transformador en la sociedad.
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 }
 
--- src/routes/QuienesSomos/historia.jsx
+++ src/routes/QuienesSomos/historia.jsx
@@ -2,34 +2,36 @@ import '/public/css/historia.css'
 function Historia() {
   return (
     <article className="container">
-      <div className="container">
-        <h2>Historia
-          <h3>Iglesia de Dios</h3>
-        </h2>
-      </div>
+      <div className="container cont1">
+        <div className="container title">
+          <h2>Historia
+            <h3>Iglesia de Dios</h3>
+          </h2>
+        </div>
 
 
 
-      <div className="container cont-historia">
-        <div className="container">
-          <p>
-            La Iglesia de Dios data su origen desde el día jueves 19 de agosto de 1886, cuando un grupo de ocho hermanos, liderados por Richard G. Spurling, ministro bautista, inconformes con la frialdad y mundanalidad de la iglesia, decidieron unirse para estudiar la Biblia, orar, vivir en santidad y experimentar el evangelio de poder.  El objetivo era “restaurar el cristianismo primitivo y lograr la unión de todas las denominaciones” (Conn, C., “Como Ejército Poderoso” 1995, p.7). Este fue un verdadero movimiento de reforma que ocurrió en el Condado Monroe, Tennessee; cerca de la frontera con Carolina del Norte.
-          </p>
+        <div className="container cont-historia">
+          <div className="container cont-txt">
+            <p>
+              La Iglesia de Dios data su origen desde el día jueves 19 de agosto de 1886, cuando un grupo de ocho hermanos, liderados por Richard G. Spurling, ministro bautista, inconformes con la frialdad y mundanalidad de la iglesia, decidieron unirse para estudiar la Biblia, orar, vivir en santidad y experimentar el evangelio de poder.  El objetivo era “restaurar el cristianismo primitivo y lograr la unión de todas las denominaciones” (Conn, C., “Como Ejército Poderoso” 1995, p.7). Este fue un verdadero movimiento de reforma que ocurrió en el Condado Monroe, Tennessee; cerca de la frontera con Carolina del Norte.
+            </p>
 
 
-          <p>
-            Después de diez años de estudio de la Biblia y de predicar la vida de santidad y la experiencia de la santificación, la pequeña iglesia  experimentó el primer derramamiento del Espíritu Santo; y así comenzó el gran movimiento pentecostal moderno.  Esto ocurrió en el año 1896.
-          </p>
+            <p>
+              Después de diez años de estudio de la Biblia y de predicar la vida de santidad y la experiencia de la santificación, la pequeña iglesia  experimentó el primer derramamiento del Espíritu Santo; y así comenzó el gran movimiento pentecostal moderno.  Esto ocurrió en el año 1896.
+            </p>
 
 
-          <p>
-            La Iglesia de Dios es la primera iglesia pentecostal de la época moderna (Walton, R. “Chronological and background Charts of Church History” 1986, chart 73). Una década después se dio el gran avivamiento de la Calle Azusa, en California, en el año 1906.
-          </p>
+            <p>
+              La Iglesia de Dios es la primera iglesia pentecostal de la época moderna (Walton, R. “Chronological and background Charts of Church History” 1986, chart 73). Una década después se dio el gran avivamiento de la Calle Azusa, en California, en el año 1906.
+            </p>
 
 
-          <p>
-            El primer nombre que le dieron a la naciente iglesia fue Unión Cristiana, en 1886. En 1902 le cambiaron el nombre a Iglesia de Santidad, por la influencia de los movimientos de santidad derivados del Metodismo wesleyano.  Finalmente, en 1907, le cambiaron el nombre a Iglesia de Dios; ese ha sido el nombre de nuestra amada iglesia hasta la fecha.
-          </p>
+            <p>
+              El primer nombre que le dieron a la naciente iglesia fue Unión Cristiana, en 1886. En 1902 le cambiaron el nombre a Iglesia de Santidad, por la influencia de los movimientos de santidad derivados del Metodismo wesleyano.  Finalmente, en 1907, le cambiaron el nombre a Iglesia de Dios; ese ha sido el nombre de nuestra amada iglesia hasta la fecha.
+            </p>
+          </div>
         </div>
       </div>
 
