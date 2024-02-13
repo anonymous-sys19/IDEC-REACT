5e7e140c197d96fcc59f9c8ed7eeff59a01edf6d
anonymous-sys19 <estudiosrg5@gmail.com>
2024-02-13 05:44:08 -0600

    new performanc

--- src/partials/serviceSection.jsx
+++ src/partials/serviceSection.jsx
@@ -31,104 +31,103 @@ const ServiceSection = () => {
                 setCurrentImagePath(randomImagePath);
             })
             .catch(error => console.error('Error fetching image paths:', error));
-    
+
 
 
         // Limpia el script al desmontar el componente
-        
+
         return () => {
-        document.getElementById('dailyVersesWrapper').removeChild(script);
+            document.getElementById('dailyVersesWrapper').removeChild(script);
+        };
+    }, []);
+
+    const dailyVersesRef = useRef(null);
+
+    const captureAndDownloadImage = async () => {
+        const canvas = await html2canvas(dailyVersesRef.current);
+        const imgData = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.href = imgData;
+        link.download = 'dailyVerses.png';
+        link.click();
     };
-}, []);
+    const dailyVerseStyle = {
+        backgroundImage: `url(${currentImagePath})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        fontWeight: 700
+        // Aquí puedes agregar otros estilos necesarios
+    };
+    return (
+        <section id="services">
 
-const dailyVersesRef = useRef(null);
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 text-center">
+                        <div className="sec-daily-father section-subheading text-muted">
+                            <section className='sec-daily' ref={dailyVersesRef} style={dailyVerseStyle}>
+                                <div className="dailyVerse" id="dailyVersesWrapper">
+                                    {/* Contenido del div */}
+                                </div>
+                            </section>
+                            <button style={
+                                {
+                                    'background': 'aquamarine',
+                                    'color': 'black',
+                                    'border': 'none',
 
-const captureAndDownloadImage = async () => {
-    const canvas = await html2canvas(dailyVersesRef.current);
-    const imgData = canvas.toDataURL('image/png');
-    const link = document.createElement('a');
-    link.href = imgData;
-    link.download = 'dailyVerses.png';
-    link.click();
-};
+                                }
+                            } onClick={captureAndDownloadImage}>Capturar imagen</button>
+                            <Helmet>
+                                <script async defer src="https://dailyverses.net/get/verse.js?language=nvi"></script>
+                            </Helmet>
+                        </div>
 
-const dailyVerseStyle = {
-    backgroundImage: `url(${currentImagePath})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    fontWeight: 700
-    // Aquí puedes agregar otros estilos necesarios
-};
-return (
-    <section id="services">
+                        {/* <iframe width="500px" scrolling="no" height="400px" frameborder="0" src="https://www.bibliatodo.com/es/online/versiculo-del-dia"></iframe> */}
 
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-12 text-center">
-                    <div className="sec-daily-father section-subheading text-muted">
-                        <section className='sec-daily' ref={dailyVersesRef} style={dailyVerseStyle}>
-                            <div className="dailyVerse" id="dailyVersesWrapper">
-                                {/* Contenido del div */}
-                            </div>
-                        </section>
-                        <button style={
-                            {
-                                'background': 'aquamarine',
-                                'color': 'black',
-                                'border': 'none',
-
-                            }
-                        } onClick={captureAndDownloadImage}>Capturar imagen</button>
-                        <Helmet>
-                            <script async defer src="https://dailyverses.net/get/verse.js?language=nvi"></script>
-                        </Helmet>
                     </div>
-
-                    {/* <iframe width="500px" scrolling="no" height="400px" frameborder="0" src="https://www.bibliatodo.com/es/online/versiculo-del-dia"></iframe> */}
-
+                </div>
+                <div className="row text-center">
+                    <div className="col-md-4">
+                        <span className="fa-stack fa-4x">
+                            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                            <i className="glyphicon glyphicon-tree-conifer"></i>
+                        </span>
+                        <h4 className="service-heading">
+                            Here is a pokok
+                        </h4>
+                        <p className="text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.
+                        </p>
+                    </div>
+                    <div className="col-md-4">
+                        <span className="fa-stack fa-4x">
+                            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                            <i className="glyphicon glyphicon-heart"></i>
+                        </span>
+                        <h4 className="service-heading">
+                            Heres a heart
+                        </h4>
+                        <p className="text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.
+                        </p>
+                    </div>
+                    <div className="col-md-4">
+                        <span className="fa-stack fa-4x">
+                            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                            <i className="glyphicon glyphicon-tint"></i>
+                        </span>
+                        <h4 className="service-heading">
+                            Waterfall maybe?
+                        </h4>
+                        <p className="text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div className="row text-center">
-                <div className="col-md-4">
-                    <span className="fa-stack fa-4x">
-                        <i className="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i className="glyphicon glyphicon-tree-conifer"></i>
-                    </span>
-                    <h4 className="service-heading">
-                        Here is a pokok
-                    </h4>
-                    <p className="text-muted">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.
-                    </p>
-                </div>
-                <div className="col-md-4">
-                    <span className="fa-stack fa-4x">
-                        <i className="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i className="glyphicon glyphicon-heart"></i>
-                    </span>
-                    <h4 className="service-heading">
-                        Heres a heart
-                    </h4>
-                    <p className="text-muted">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.
-                    </p>
-                </div>
-                <div className="col-md-4">
-                    <span className="fa-stack fa-4x">
-                        <i className="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i className="glyphicon glyphicon-tint"></i>
-                    </span>
-                    <h4 className="service-heading">
-                        Waterfall maybe?
-                    </h4>
-                    <p className="text-muted">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section >
-)
+        </section >
+    )
 }
 
 export default ServiceSection;
